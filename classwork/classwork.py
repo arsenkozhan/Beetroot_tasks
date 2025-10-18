521507a24048d6eef6b3a66d24d3fcd55bc1e3b9
Arsen Kozhan <arsenn.kozhann@gmail.com>
2025-10-18 11:24:12 +0300

    homework1

--- classwork/classwork.py
+++ classwork/classwork.py
@@ -1,1 +1,12 @@
-print("Hello world!")
+# print("hello world")
+# print(1,2,3,4,5)
+#
+# print(1, end=' ')
+# print(2, end=' ')
+# print(3, end=' ')
+
+print("Hello World")
+print("Hello World")
+print("Hello World")
+
+print(1, 2, 3, 4, 5)
