d24cdc6a11faabb957e214bf52771a6209e367de
Arsen Kozhan <arsenn.kozhann@gmail.com>
2025-10-20 19:08:54 +0300

    hw2_18.10

---
+++ homework/hw2_18.10.py
@@ -0,0 +1,8 @@
+name = "Arsen"
+print("Hello", name)
+print("Hello", " my", " name", " is", " Arsen", sep = ",")
+print("Hello, my name is Arsen", end = "")
+print(" My surname is Kozhan", end = "\n\n")
+print(" I`m 25 years")
+print("#####", "\n" "#\t#\n" "#\t#\n" "#\t#\n" "#####\n", end = '\n')
+print("#\t#\n" "#\t#\n" "####" "#\t\n" "#\t#\n" "#\t#\n")
