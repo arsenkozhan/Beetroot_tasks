f3fc31ce0af6ee7efb954a31fce54cc707e9bf4f
Arsen Kozhan <arsenn.kozhann@gmail.com>
2025-10-20 09:32:14 +0300

    hw3_18.10

---
+++ homework/hw3_18.10.py
@@ -0,0 +1,24 @@
+
+print("Task 1")
+name = "Arsen"
+day = "Monday"
+print(f"Good day, {name}! {day} is a perfect day to learn some python.")
+print("Good day, %s! %s is a perfect day to learn some python." % (name, day), end="\n\n")
+
+print("Task 2")
+first_name = "Arsen"
+last_name = "Kozhan"
+print(first_name, last_name, sep=" ")
+print(first_name + " " + last_name)
+print(f"{first_name} {last_name}", end="\n\n")
+
+print("Task 3")
+a = 3
+b = 2
+print(a + b)
+print(a - b)
+print(a / b)
+print(a * b)
+print(a ** b)
+print(a // b)
+print(a % b)
