9cf430a90e9900e7f68c5a41d8a0b631ddfa8563
arsenkozhan <arsenn.kozhann@gmail.com>
2025-10-24 12:18:09 +0300

    hw5-6

---
+++ homework/hw5-6_25.10.py
@@ -0,0 +1,67 @@
+# 5. Input та швидкий огляд імпортів
+# print("Task 1")
+# import random
+# rand = random.randint(1, 10)
+# user = int(input("Guess a number between 1 and 10: "))
+# print(rand)
+# if rand == user:
+#     print("You guessed right!")
+# else:
+#     print("You guessed wrong!")
+
+
+# print("Task 2")
+# name = input("What is your name? ")
+# age = int(input("How old are you? "))
+# print(f"Hello {name}, on your next birthday you’ll be {age+1} years")
+
+# print("Task 3")
+# import random
+# word = input("Enter a word: ")
+# for i in range(len(word)):
+#     random_word = "".join(random.sample(word, len(word)))
+#     print(random_word)
+
+
+
+
+
+
+#  
+# Task 3
+# Extracting numbers.
+# Make a list that contains all integers from 1 to 100, then find all integers from the list that are divisible by 7 but not a multiple of 5, and store them in a separate list. Finally, print the list.
+# Constraint: use only while loop for iteration
+
+
+# 6. Списки, кортежі та множини
+print("Task 1")
+# numbers = []
+# count = 0
+# while count < 10:
+#     number = random.randint(1, 20)
+#     numbers.append(number)
+#     count += 1
+# print("List of numbers:", list(numbers))
+# print("Max element in the list ", max(numbers))
+
+
+# Task 2
+# Exclusive common numbers.
+# Generate 2 lists with the length of 10 with random integers from 1 to 10, and make a third list containing the common integers between the 2 initial lists without any duplicates.
+# Constraints: use only while loop and random module to generate numbers
+print("Task 2")
+numbers1 = []
+numbers2 = []
+numbers3 = []
+count = 0
+while count < 10:
+#     number = random.randint(1, 10)
+#     numbers1.append(number)
+#     numbers2.append(number)
+#     numbers3 = numbers1 + numbers2
+#     count += 1
+#     break
+#
+# print(numbers3)
+
