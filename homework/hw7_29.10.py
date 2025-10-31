c149b44d7fb10b28b652a0d6441c8268a420db83
arsenkozhan <arsenn.kozhann@gmail.com>
2025-10-31 11:07:57 +0200

    hw7_29.10

---
+++ homework/hw7_29.10.py
@@ -0,0 +1,42 @@
+# Task 1
+# sentence = input("Enter a sentence: ")
+# words = sentence.split()
+# word_count = {}
+# for word in words:
+#     word_count[word] = word_count.get(word, 0) + 1
+# print(words)
+
+# Task 2
+# Input data:
+# stock = {"banana": 6, "apple": 0, "orange": 32, "pear": 15}
+# prices = {"banana": 4, "apple": 2, "orange": 1.5, "pear": 3}
+#
+# total_prices = {}
+#
+# for item in stock:
+#     item_total = stock[item] * prices[item]
+#     total_prices[item] = item_total
+#
+# print(total_prices)
+
+# Task 3
+result = [(i, i**2) for i in range(1, 11)]
+# for i in range (1, 11):
+#     j = i ** 2
+#     result.append(j)
+#     print(j)
+print(result)
+
+
+# Task 4
+# days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+# week_dict = {}
+# reverse_dict = {}
+# for i in range(7):
+#     week_dict[i + 1] = days[i]
+# for key, value in week_dict.items():
+#     reverse_dict[value] = key
+# print(reverse_dict)
+# print(week_dict)
+
+
